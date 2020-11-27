1c6d56f6fd7b2f7016996e68a528f1390b01c2cb
st4rlight <st4rlight@163.com>
2020-11-28 00:44:39 +0800

    change project hierarchy

---
+++ blog-service/src/main/java/cn/st4rlight/blogservice/BlogServiceApplication.java
@@ -0,0 +1,13 @@
+package cn.st4rlight.blogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogServiceApplication.class, args);
+	}
+
+}
---
+++ blog-service/src/test/java/cn/st4rlight/blogservice/BlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.st4rlight.blogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
